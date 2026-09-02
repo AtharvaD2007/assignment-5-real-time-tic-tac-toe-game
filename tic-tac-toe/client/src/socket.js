9b1fe1af54f62bb233c5de3b07f3c076ba259b81
Atharva Dalvi <atharvaadalvi2007@gmail.com>
2026-09-02 11:54:10 +0530

    Fix Socket.IO connection

--- tic-tac-toe/client/src/socket.js
+++ tic-tac-toe/client/src/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const socket = io(
     import.meta.env.VITE_API_URL,
     {
-        transports: ["websocket", "polling"],
+        transports: ["polling"],
         reconnection: true,
         reconnectionAttempts: Infinity,
         reconnectionDelay: 1000
